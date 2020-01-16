7d8edc48e8eda3ecdeef147122c9ca42fa6ac51c
SAHLER Vincent <33986208+CofeDrink68@users.noreply.github.com>
2020-01-16 21:10:32 +0100

    Add files via upload

---
+++ client.py
@@ -0,0 +1,133 @@
+import socket # On importe socket, une librairie pour le tcp/ip
+import time # On importe time, une librairie de temps
+import os # On importe os, la librairie système
+import chess # On importe chess, une librairie d'échec
+import json # On importe json, une librairie pour traiter du json
+
+linux = False # Est ce que nous sommes sous linux ?
+
+def clear(): # On définit la fonction pour nettoyer le terminal
+    os.system("clear" if linux else "cls") # On execute cls sour windows ou clear sous linux
+
+def standard():
+    print("*************************************") 
+    print(" ****  ****  *  *  ****  ****  /****/")
+    print(" **   *      ****  **   *     /****\\")  
+    print(" ****  ****  *  *  ****  ****   \***\\") 
+    print("*************************************") 
+
+ip = input("Adresse IP du serveur : ") # On demande l'IP du serveur
+port = input("Le port du serveur : ") # On demande le port du serveur
+
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # On initialise un socket
+    s.connect((ip, int(port))) # On essaye de se connecter
+except:
+    print("Une erreur s'est produite ...") # Une erreur s'est produire lors de la connection au serveur
+    exit(1) # On quitte
+
+clear() # On appelle le fonction clear qui nettoie le terminal 
+print("Connecté au serveur\n") # On affiche que l'on est connecté au serveur
+input("Appuyez sur entrée pour continuer") # On demande au client d'appuyer sur entrée afin de pouvoir jouer
+
+choix = -1 # Choix impossible
+while choix == -1: # Tant que rien n'est choisi
+    clear() # On appelle encore la fonction clear toujours pour nettoyer le terminal
+    standard() # On affiche "Echecs"
+    print() # On saute une ligne parce-que flemme de \n
+    print("[1] Créer une nouvelle partie") # On affiche que l'on crée une nouvelle partie 
+    print("[2] Rejoindre") # On affiche l'option rejoindre 
+    print() # On saute une ligne parce-que flemme de \n
+    choix = input("$ ") # On lit la réponse de l'utilisateur
+    try: # On essaye
+        choix = int(choix) # On essaye de transformer <str>choix en <int>choix
+        if(choix == 1 or choix == 2): # On teste que c'est bien 1 ou 2
+            pass # On passe
+        else: # Sinon
+            int("quarante-deux") # On génère une erreur ;)
+    except:
+        print("Une erreur est survenue ...") # On affiche qu'il y a eu une erreur 
+        choix = -1 # Toujours pas choisi
+
+if choix == 2: # Si on rejoint une partie
+    print() # Nouvelle ligne
+    _id = input("ID de la partie : ") # On importe l'ID de la partie 
+    s.send(json.dumps({"party": ("new" if choix == 1 else "join"), "link": _id}).encode()) # Si le choix = 1 on envoie new au client sinon on envoie join
+    # Est-ce qu'il y a une erreur ?
+    message = s.recv(2048) # On prend le premier messgae qui est en attente
+    message = json.loads(message.decode()) # On décode le message
+    if message["error"] == 0: # Si il n'y a pas d'erreur
+        print("\nLien valide") # On affiche que le lien est valide
+        # On continue
+    else: # Si il y a eu une erreur
+        print("Une erreur est apparue, code {}".format(message["error"])) # Pour être explicite
+        exit(1) # On quitte
+    input("Appuyez sur entrée pour continuer") # On demande au client d'appuyer sur entrée afin de pouvoir jouer
+else: # Si on crée une partie
+    s.send(json.dumps({"party": ("new" if choix == 1 else "join")}).encode()) # On envoire qu'on veut créer une partie
+    m = s.recv(4096) # On reçoit la réponse
+    m = json.loads(m.decode()) # On la décode
+    print("Id de partie : "+str(m["id"])) # On affiche l'ID de la partie
+    # On continue
+    input("Appuyez sur entrée pour continuer") # On demande au client d'appuyer sur entrée afin de pouvoir jouer
+
+m__ = s.recv(2048).decode().split("}") # Deux json en un, pour les sepparer
+m = json.loads(m__[0]+"}") # On prend le premier 
+
+color = m # On récupère la couleur
+
+party = {"stop": False} # POur eviter les erreures :)
+
+first = True # Pour savoir si on doit attendre le serveur ou si on doit prendre la deuxième partie du message ligne 74
+
+while party["stop"] == False: # Tant que pas échech
+    if first: # Si c'est la première fois
+        m = m__[1]+"}" # On formatte
+        m = json.loads(m) # On importe
+        first = False # On evite de repasser là la prochaine fois
+    else: # Si c'est une fois déjà experimentée 
+        m = s.recv(2048) # On attends pour un message
+        m = json.loads(m.decode()) # On le décode
+
+    if m["is2Play"] == color["color"]: # Si c'est à noud de jouer
+        error = True # Pour entrer dans la boucle
+        while error: # Tant que le serveur renvoie qu'il y a une erreur
+            clear() # On clear
+            standard() # On affiche "echecs"
+            print() # On saute une ligne
+            print(chess.Board(m["board"])) # On affiche le plateau imbuvable
+            print() # On saute une ligne
+            print("Quel mouvement voulez-vous effectuer ?") # On demande pour un mouvement
+            move = input("$ ") # On prend le mouvement
+            s.send(json.dumps({"move": move}).encode()) # Et on l'envoie
+
+            m_ = s.recv(2048) # On attend la réponse du serveur (est-ce qu'il y a un mouvement illégal)
+            m_ = json.loads(m_.decode()) # On décode le message
+
+            if m_["error"] == 0: # Si ya pas d'erreur
+                error = False # Ben y'a pas d'erreur (sortie de la boucle)
+            else: # Si il y a un mouvement illégal
+                print("Une erreur est survenue de type "+str(m_["error"])) # On affichie qu'il y a une erreur
+                input("Appuyez sur entrée pour continuer") # On demande au client d'appuyer sur entrée afin de pouvoir jouer
+        m_ = s.recv(2048) # On attend le message du serveur pour le prochain tour
+        m_ = json.loads(m_.decode()) # Et on le décode
+    else: # Si c'est pas a nous de jouer
+        clear() # On clear
+        standard() # On affiche echecs
+        print() # On saute une ligne
+        print(chess.Board(m["board"])) # On affiche le plateau
+        print() # On saute une ligne
+        print("En attente de l'autre joueur") # ...  je psense que c'est assez explixite
+        m_ = s.recv(2048) # On attend la réponse du serveur pour le prochain tour
+        m_ = json.loads(m_.decode()) # On le décode
+    
+    # print(m_)
+    party["stop"] = m_["over"] # On regarde si c'est echec et mat
+
+clear() # Partie finie, on clear
+standard() # On affiche echecs
+print("\nPartie finie") # ... evident
+print("\n"+chess.Board(m["board"])) # On affiche le plateau (notez que j'ai eu la foi de faire un vrai saut de ligne ;) )
+input("\nAppuyez sur entrée pour continuer") # On demande au client d'appuyer sur entrée afin de pouvoir jouer
+s.close() # On ferme la connectiobn sans aucun scrupule
+clear() # On clear le terminal
---
+++ server.py
@@ -0,0 +1,177 @@
+import threading # on importe threading, une librairie qui permet du faire du multithread
+import socket    # on importe socket, une libraire pour le tcp/ip
+import chess     # on importe chess, une librairie moteur d'échec
+import json      # on importe json, une librairue pour traiter du json
+import random    # on importe random, une librairie pour générer des nombres aléatoires
+import time      # on importe time, une librairie de temps
+
+class Game(threading.Thread): # on initialise une nouvelle classe "Game"
+    def __init__(self): # Fonction d'initialisation
+        threading.Thread.__init__(self)
+        self.board = chess.Board() # On initie un plateau
+        self.id = (int(time.time()*100) ^ random.randint(1, 1000)) # on génère une clé avec du xor
+        # On initialise des variables
+        self.isWhiteUsed = None
+        self.isBlackUsed = None
+        self.tour = 0 # %2==0 is that it's white to play
+    
+    def run(self):
+        while (self.isWhiteUsed is None) or (self.isBlackUsed is None): # On attend que les deux soient connectés
+            pass
+
+        self.begin() # On initialise
+
+    def getColor(self, conn): # On définit la fonction qui désigne la couleur 
+        if(self.isWhiteUsed is None and self.isBlackUsed is None): # on regarde si ni les blancs ni les noirs sont pris
+            x = random.randint(0, 1) # On pioche au hasard
+            if x: # Si on a choisi 1 donc noir
+                self.isBlackUsed = conn # On affecte la connection à la variable
+                return 1 # On renvoie qu'on a choisi noir
+            else: # Sinon on a choisi blanc
+                self.isWhiteUsed = conn # On affecte la connection à la variable
+                return -1 # On renvoie qu'on a choisi blanc
+        elif(self.isBlackUsed is None): # Si le noir n'est pas utilisé (et que les blancs sont donc pris)
+            self.isBlackUsed = conn # On affecte la connection à la variable
+            return 1 # On renvoie que l'utilisateur sera noir
+        elif(self.isWhiteUsed is None): # Si le blanc n'est pas utilisé (et que les noirs sont donc pris)
+            self.isWhiteUsed = conn # On affecte la connection à la variable
+            return -1 # On renvoie que l'utilisateur sera blanc
+        else: # C'est que déjà deux joueurs sont présents
+            return 0 # On renvoie une erreur
+    
+    def move(self, color, moveName): # Fonction pour commander une pièce
+        if not(self.isWhiteUsed is None) and not(self.isBlackUsed is None): # Est ce que les deux joueurs sont présents  
+                if color == (-1 if self.tour%2==0 else 1): # Est ce que c'est à la bonne personne de jouer
+                    if not(self.board.is_game_over()):
+                        if chess.Move.from_uci(moveName) in self.board.legal_moves: # Est ce que le mouvement est possible
+                            m = chess.Move.from_uci(moveName) # On inscrit le mouvement
+                            self.board.push(m) # On le mets dans le plateau
+                            self.tour += 1 # On incrémente le tour
+
+                            if color == -1: # Si couleur blanche
+                                self.isWhiteUsed.send(json.dumps({"error": 0}).encode()) #dire qu'il n'y a pas d'erreur
+                            else: # Sinon
+                                self.isBlackUsed.send(json.dumps({"error": 0}).encode()) # dire qu'il n'y a pas d'erreur
+
+                            return 0 # On renvoie que c'est bon
+                        else: # Si le mouvement est illégal
+                            if not(0 if self.tour%2==0 else 1): # Si c'est aux blancs
+                                self.isWhiteUsed.send(json.dumps({"over": self.board.is_game_over(), "error": 4, "board": self.board.fen(), "is2Play": (-1 if self.tour%2==0 else 1)}).encode()) # On lui envoie une erreur 4
+                            else: # Si c'est au noirs
+                                self.isBlackUsed.send(json.dumps({"over": self.board.is_game_over(), "error": 4, "board": self.board.fen(), "is2Play": (-1 if self.tour%2==0 else 1)}).encode()) # On lui envoie une erreur 4
+                            return 4 # On return l'error
+                    else: # Si c'est echec 
+                        pass # Ben on s'en fout
+
+                    message = {"board": self.board.fen(), "is2Play": (-1 if self.tour%2==0 else 1)} #  On envoie l'objet du plateau
+                    message["over"] = self.board.is_game_over() # On ajoute l'echec et mat
+
+                    self.isBlackUsed.send(json.dumps(message).encode()) # On envoie le message au client noir
+                    self.isWhiteUsed.send(json.dumps(message).encode()) # On envoie le message au client blanc
+        else: # Si un des utilisateur est pas connecté
+            self.isWhiteUsed.send(json.dumps({"over": self.board.is_game_over(), "error": 3, "board": self.board.fen(), "is2Play": (-1 if self.tour%2==0 else 1)}).encode()) # On lui envoie une erreur 3
+            self.isBlackUsed.send(json.dumps({"over": self.board.is_game_over(), "error": 3, "board": self.board.fen(), "is2Play": (-1 if self.tour%2==0 else 1)}).encode()) # On lui envoie une erreur 3
+            return 3 # Erreur 3 (errors.txt)
+    
+    def begin(self):
+        message = {"board": self.board.fen(), "is2Play": (-1 if self.tour%2==0 else 1)} #  On envoie l'objet du plateau
+        self.isBlackUsed.send(json.dumps(message).encode()) # On envoie le message au client noir
+        self.isWhiteUsed.send(json.dumps(message).encode()) # On envoie le message au client blanc
+
+class Connection(threading.Thread): # On crée une classe Multithread qui gère UNE connection via TCP/IP
+    def __init__(self, conn, addr, id): # on définit la fonction utilisation
+        threading.Thread.__init__(self) # On configure le thread
+        # On copie les variables
+        self.conn = conn
+        self.addr = addr
+        self.id = id
+        # On initialise les variables
+        self.gameId = None
+        self.gameObject = None
+        self.black_white = None
+        self.stop = False # On stop le thread
+
+    def run(self): # Lancement du thread
+        while (self.gameId is None) or (self.gameObject is None): # On attend d'avoir un game ID
+            pass
+
+        self.black_white = self.gameObject.getColor(self.conn) # On obtient la couleur
+
+        if(self.black_white == 0): # Si la fonction renvoie 0 c'est que le jeu est déja satisfait
+            self.conn.send(json.dumps({"error": 1}).encode()) # Donc on envoie le code 1 (errors.txt) au client
+        else: # Couleur choisie
+            self.conn.send(json.dumps({"error": 0, "color": self.black_white}).encode()) # On envoie au client que c'est bon
+
+        while (self.gameObject.isWhiteUsed is None) or (self.gameObject.isBlackUsed is None): # On attend qu'ils soient connectés
+            pass
+
+        while not self.stop: # Tant que ce n'est pas stoppé
+            message = self.conn.recv(2048) # On récupère un message du client
+            print(message) # On débug :)
+            if message.decode() != "": # Si c'est pas vide
+                message = json.loads(message.decode()) # On le décode
+            else: # Sinon
+                message = {"move": None} # On s'en fout et on évite les bugs
+            if(message["move"] is not None): # Si on lui demande de bouger quelque chose
+                result = self.gameObject.move(self.black_white, message["move"])#On appelle la fonction move
+                renvoi = {} # On initialise la variable renvoi
+                renvoi["over"] = self.gameObject.board.is_game_over() # On ajoute si mat
+                renvoi["board"] = self.gameObject.board.fen() # On renvoie le plateau
+                renvoi["error"] = result # On renvoie le message d'erreur
+                print(renvoi) # On débug ;)
+                # self.conn.send(json.dumps(renvoi).encode()) # On utilise la connexion du client pour lui envoyer la variable renvoi.
+                self.gameObject.isWhiteUsed.send(json.dumps(renvoi).encode()) # On envoie le message
+                self.gameObject.isBlackUsed.send(json.dumps(renvoi).encode()) # Idem
+                self.gameObject.begin() # On utilise begin() comme il faut pas XD
+            else: # Si le message ne contient pas de mouvement
+                self.conn.send(json.dumps({"error": 4}).encode()) # On envoie l'erreur
+
+class MainThread(threading.Thread): # On crée jne classe multiThread qui est le coeur du système
+    def __init__(self, port=554): # On définit la fonction d'initialisation
+        threading.Thread.__init__(self) # On définit le thread
+        # On copie la variable port mais pas trop globale :)
+        self.port = port
+        
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # On initialise le socket
+        self.sock.bind(("127.0.0.1", self.port)) # On affecte le port au socket
+
+        # Initialisation des variables
+        self.stop = False
+        self.accept = True 
+        self.games = {}
+        self.connections = []
+
+    def run(self): # On définit la fonction principal
+        self.sock.listen(1) # On commence écouter
+        while not self.stop: # Tant que le thread n'est pas fini
+            if self.accept: # Si on accepte :
+                conn, addr = self.sock.accept() # On attends la venu d'une connection et on accepte
+                self.connections.append(Connection(conn, addr, len(self.connections))) # On ouvre une nouvelle instance de Connection et  on la mets dans la variable self.connections
+                self.connections[-1].start() # On démare l'instance de connection
+                message = conn.recv(2048) # On prends le message du client qui contient si on ouvre ou on crée une partie
+                message = json.loads(message.decode()) # On convertit la chaîne de caractère formatée JSON en tableau
+                if message["party"] == "new": # Si on crée une partie
+                    gInst = Game() # On initialise une nouvelle instance de Game
+                    gInst.start() # On démarre le thread
+                    conn.send(json.dumps({"id": gInst.id}).encode()) # On envoie l'ID
+                    self.games[gInst.id] = gInst # On renseigne le moteur de jeu grace à son id
+                    self.connections[-1].gameObject = gInst # On l'affecte à la connection
+                    self.connections[-1].gameId = gInst.id # Ainsi que l'ID de partie
+                elif message["party"] == "join": # Si on joint une partie
+                    _pass = True # Retour du try:
+                    link = 0 # POur eviter les problèmes
+                    try: # On essaye
+                        link = int(message["link"]) # de transformer en int
+                    except: # Si qq passe pas bien
+                        _pass = False # On le sauvegarde
+                    if link in self.games and _pass: # Si l'id de partie est connu et pas d'erreur
+                        conn.send(json.dumps({"error": 0}).encode()) # On renvoie au client qu'il n'y a pas d'erreur
+                        self.connections[-1].gameObject = gInst # On l'affecte à la connection
+                        self.connections[-1].gameId = gInst.id # Ainsi que l'ID de partie
+                    else: # Sinon
+                        conn.send(json.dumps({"error": 5}).encode()) # Erreur (pour changer)
+
+
+if __name__ == "__main__": # Si le code est éxecuté et pas ouvert par un autre programme python
+    mt = MainThread(port=554) # On ouvre une instance de MainThread sur le port 554
+    mt.start() # et on la démarre
